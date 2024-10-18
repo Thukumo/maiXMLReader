0ac33e90a9ab0f85344afb34eafa7096bfc49da7
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-18 18:05:12 +0900

    なんかいろいろ足した

--- main.py
+++ main.py
@@ -1,27 +1,65 @@
-import os, csv, xml.etree.ElementTree, sys
+import os, csv, xml.etree.ElementTree, sys, itertools
 
 def files(path):
     for path, _, files in os.walk(path):
         for file in files: yield path, file
 
-lines = []
+if sys.version_info.major != 3 or sys.version_info.minor < 10:
+    print("このメッセージが出たら伝えてください\n修正します")
+    exit()
+fumens = []
+dx_fumens = []
+utage_fumens = []
 #カレントディレクトリ以下のすべてのファイルから、ファイル名がMusic.xmlのものを取得し、そのデータを読む
 for path, file in files("."):
     if file == "Music.xml":
         root = xml.etree.ElementTree.parse(os.path.join(path, file)).getroot()
         song_id, song_name = root.find("name").find("id").text, root.find("name").find("str").text
         artist_name = root.find("artistName").find("str").text
-        lines.append([int(song_id), song_name, artist_name])
+        match(len(song_id)):
+            case 1 | 2 | 3 | 4:
+                fumens.append([int(song_id), song_name, artist_name])
+            case 5:
+                dx_fumens.append([int(song_id[1:]), song_name, artist_name])
+            case 6:
+                utage_fumens.append([int(song_id[2:]), song_name, artist_name])
+            case _:
+                print(f"Failed to switch: {song_id}")
         #print(f"Song ID: {song_id}\nSong Name: {song_name}\nArtist Name: {artist_name}")
 
-if True: #ここがTrueの場合、データのなかったIDを空欄で埋める(Falseで埋めない)
-    memo = set(l[0] for l in lines)
-    for i in range(1, max(memo)):
-        if i not in memo: lines.append([i, "", ""])
 
+fill_brank = True #ここがTrueの場合、データのなかったIDを空欄で埋める(Falseで埋めない)
+
+lines = []
+marks = {}
+tmp = fumens + dx_fumens + utage_fumens
+memo = set(l for l in range(max(tmp, key=lambda x: x[0])[0]))
+for i in range(1, max(memo)):
+    if fill_brank and i not in tmp: lines.append([i, "", ""])
+
+#譜面の種類ごとのデータをそれっぽい感じに結合
+hoge = [fumens, dx_fumens, utage_fumens]
+for (i, j) in itertools.combinations(range(3), 2):
+    tmp = [hoge[j][k][0] for k in range(len(hoge[j]))]
+    for fumen in hoge[i]:
+        if fumen[0] not in marks: marks[fumen[0]] = [fumen, set([i])]
+        else: marks[fumen[0]][1].add(i)
+        if fumen[0] in tmp: marks[fumen[0]][1].add(j)
+
+#宴譜面名の抽出とか
+tmp = [fumen[0] for fumen in utage_fumens]
+def get_name(id):
+    ret_lis = []
+    for i in range(len(tmp)):
+        if tmp[i] == id: ret_lis.append(i)
+    return ", ".join(list(map(lambda x: utage_fumens[x][1], ret_lis)))
+for fumen in marks.values():
+    normal_exists, dx_exists, utage_exists = 0 in fumen[1], 1 in fumen[1], 2 in fumen[1]
+    lines.append(fumen[0]+["×" if not normal_exists else "〇", "×" if not dx_exists else "〇", "×" if not utage_exists else "〇", get_name(fumen[0][0]) if utage_exists else ""])
 lines.sort(key=lambda x: x[0])
+
 #CSVファイルに書き込む(存在する場合は上書き)
 with open(sys.argv[1] if len(sys.argv) == 2 else "Music.csv", "w", newline="", encoding="utf-16") as f:
     writer = csv.writer(f, delimiter="\t")
-    writer.writerow(["Song ID", "Song Name", "Artist Name"])
+    writer.writerow(["Song ID", "タイトル", "アーティスト名", "スタンダード", "DX", "宴", "宴譜面名"])
     for l in lines: writer.writerow(l)
