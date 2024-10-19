0d0903ce5948919a997089214540719072be3600
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-19 20:17:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -26,37 +26,28 @@ for path, file in files("."):
             case _:
                 print(f"IDから譜面のタイプを識別できませんでした: {song_id=}")
 
-
 fill_brank = True #ここがTrueの場合、データのなかったIDを空欄で埋める(Falseで埋めない)
 
-lines = []
-if fill_brank:
-    tmp = set([l[0] for l in fumens + dx_fumens + utage_fumens])
-    lines = [[i, "", "", "", "", "", ""] for i in range(1, max(tmp)) if i not in tmp]
-
-#楽曲のリストをいい感じに結合
-marks = {}
-fumen_lists = [fumens, dx_fumens, utage_fumens]
-for (i, j) in itertools.combinations(range(len(fumen_lists)), 2):
-    tmp = set(fumen_lists[j][k][0] for k in range(len(fumen_lists[j])))
+#データの集計
+used_id = set(l[0] for l in fumens + dx_fumens + utage_fumens)
+lines = [[i+1, "", "", "×", "×", "×", []] for i in range(max(used_id))]
+fumen_lists = [fumens, dx_fumens]
+for i in range(len(fumen_lists)):
     for fumen in fumen_lists[i]:
-        if fumen[0] not in marks: marks[fumen[0]] = [fumen, set([i])]
-        else: marks[fumen[0]][1].add(i)
-        if fumen[0] in tmp: marks[fumen[0]][1].add(j)
+        lines[fumen[0]-1][1:3] = fumen[1:3]
+        lines[fumen[0]-1][3+i] = "〇"
+for fumen in utage_fumens:
+    lines[fumen[0]-1][5] = "〇"
+    lines[fumen[0]-1][6].append(fumen[1])
+for line in lines: line[6] = ", ".join(line[6])
+if fill_brank:
+    for i in range(1, len(lines)+1):
+        if i not in used_id: lines[i-1] = [i, "", "", "", "", "", ""]
+else: lines = [lines[i] for i in range(len(lines)) if i+1 in used_id]
 
-#宴譜面名の抽出とか
-tmp = [fumen[0] for fumen in utage_fumens]
-for fumen in marks.values():
-    normal_exists, dx_exists, utage_exists = 0 in fumen[1], 1 in fumen[1], 2 in fumen[1]
-    lines.append(fumen[0]+["×" if not normal_exists else "〇",
-"×" if not dx_exists else "〇",
-"×" if not utage_exists else "〇",
-", ".join(map(lambda x: utage_fumens[x][1], [i for i in range(len(tmp)) if tmp[i] == fumen[0][0]])) if utage_exists else ""])
-
-lines.sort(key=lambda x: x[0])
 #CSVファイルに書き込む(存在する場合は上書き)
 try:
-    with open(sys.argv[1] if len(sys.argv) == 2 else "Music.csv", "w", newline="", encoding="utf-16") as f:
+    with open(sys.argv[1] if len(sys.argv) == 2 else "maimai.csv", "w", newline="", encoding="utf-16") as f:
         writer = csv.writer(f, delimiter="\t")
         writer.writerow(["Song ID", "タイトル", "アーティスト名", "ST", "DX", "宴", "宴譜面名"])
         for l in lines: writer.writerow(l)
