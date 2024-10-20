768d9276483b3b74c83e8e61c328171d17435236
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-20 12:55:06 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -9,28 +9,30 @@ fumen_lists = [[], [], []] #ST, DX, 宴
 for path, file in files("."):
     if file == "Music.xml":
         root = xml.etree.ElementTree.parse(os.path.join(path, file)).getroot()
-        song_id, song_name = root.find("name").find("id").text, root.find("name").find("str").text
-        artist_name = root.find("artistName").find("str").text
+        song_id = root.find("name").find("id").text
         num = 0 if len(song_id) < 5 else len(song_id)-3 -1
-        fumen_lists[num].append([int(song_id[num:]), song_name, artist_name, num+3])
+        fumen_lists[num].append([int(song_id[num:]), root.find("name").find("str").text, root.find("artistName").find("str").text, num+3])
 
 fill_brank = True #ここがTrueの場合、データのなかったIDを空欄で埋める(Falseで埋めない)
 
 #データの集計
 used_id = {l[0] for fumen_list in fumen_lists for l in fumen_list}
-lines = [[i+1, "", "", "", "", "", []] for i in range(max(used_id))]
+lines = [[i+1, None, "", "", "", "", []] for i in range(max(used_id))]
 for lis in fumen_lists:
-    n = lis[0][3] #要素が0であることはないと思うからこれで
+    try: n = lis[0][3]
+    except IndexError: pass #宴譜面の前例があった
     for fumen in lis:
         ind = fumen[0]-1
         if n == 5: lines[ind][6].append(fumen[1])
-        if lines[ind][1] == "":
+        if lines[ind][1] is None:
             lines[ind][1:3] = fumen[1:3]
-            #宴譜面しか譜面がない場合に先頭の"[(漢字1文字)]"を消すための処理 (公開前 and 宴譜面しかない)楽曲かつ変な曲名だと曲名が欠ける(公開前の宴譜面には先頭に"[漢字1文字]"がつかないため)
+            #宴譜面しか譜面がない場合に曲名先頭の"[(漢字1文字)]"を消すための処理 (公開前 and 宴譜面しかない)楽曲かつ変な曲名だと曲名が欠ける(公開前の宴譜面の楽曲名には[(漢字)]がつかないため)
             if n == 5 and bool(re.fullmatch(r"^\[[\u4E00-\u9FFF]\]", fumen[1][:3])): lines[ind][1] = lines[ind][1][3:]
         lines[ind][n] = "〇"
 if not fill_brank: lines = [line for i, line in enumerate(lines) if i+1 in used_id]
-for line in lines: line[6] = ", ".join(line[6])
+for line in lines:
+    if fill_brank and line[0] not in used_id: line[1] = ""
+    line[6] = ", ".join(line[6])
 
 #CSVファイルに書き込む(存在する場合は上書き)
 try:
