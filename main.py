1a58d6a6e6fa99128d5b8ed11e636f59ebc8fe2e
つくも <92912896+Thukumo@users.noreply.github.com>
2024-10-22 01:50:35 +0900

    未使用、削除譜面を読んだ際にST譜面として認識される問題を修正

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ def files(path):
     for path, _, files in os.walk(path):
         for file in files: yield path, file
 
-fumen_lists = [[], [], []] #ST, DX, 宴
+fumen_lists = [[], [], [], []] #ST, DX, 宴, 未使用 or 削除譜面
 #カレントディレクトリ以下のすべてのファイルから、ファイル名がMusic.xmlのものを取得し、そのデータを読む
 for path, file in files("."):
     if file == "Music.xml":
         root = xml.etree.ElementTree.parse(os.path.join(path, file)).getroot()
         song_id = root.find("name").find("id").text
         num = 0 if len(song_id) < 5 else len(song_id)-3 -1
-        fumen_lists[num].append([int(song_id[num:]), root.find("name").find("str").text, root.find("artistName").find("str").text, num+3])
+        if any(notes.find("isEnable").text == "true" for notes in root.find("notesData").findall("Notes")):
+            fumen_lists[num].append([int(song_id[num:]), root.find("name").find("str").text, root.find("artistName").find("str").text, num+3])
+        else:
+            fumen_lists[3].append([int(song_id[num:]), root.find("name").find("str").text, root.find("artistName").find("str").text, 999])
 
 fill_brank = True #ここがTrueの場合、データのなかったIDを空欄で埋める(Falseで埋めない)
 fill_all_status = False #存在しなかった譜面の欄に×を表示するかどうか
 
 #データの集計
-used_id = {l[0] for fumen_list in fumen_lists for l in fumen_list}
+used_id = {l[0] for list in fumen_lists for l in list}
 lines = [[i+1, None, "", "", "", "", []] for i in range(max(used_id))]
 for lis in fumen_lists:
     try: n = lis[0][3]
@@ -29,7 +32,7 @@ for lis in fumen_lists:
             lines[ind][1:3] = fumen[1:3]
             #宴譜面しか譜面がない場合に曲名先頭の"[(漢字1文字)]"を消すための処理 (公開前 and 宴譜面しかない)楽曲かつ変な曲名だと曲名が欠ける(公開前の宴譜面の楽曲名には[(漢字)]がつかないため)
             if n == 5 and bool(re.fullmatch(r"^\[[\u4E00-\u9FFF]\]", fumen[1][:3])): lines[ind][1] = lines[ind][1][3:]
-        lines[ind][n] = "〇"
+        if n != 999: lines[ind][n] = "〇"
 
 if fill_all_status:
     for i in used_id: lines[i-1][2:5] = ["〇" if lines[i-1][j] == "〇" else "×" for j in range(2, 5)]
